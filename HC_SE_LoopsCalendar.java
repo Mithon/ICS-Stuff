8ec249c76426f5d0120728d23716ccb38a7b296a
Mithon <huntercaron@mac.com>
2013-10-24 14:43:11 -0400

    Update 2
    I think its closer now

--- HC_SE_LoopsCalendar.java
+++ HC_SE_LoopsCalendar.java
@@ -11,9 +11,9 @@ public class HC_SM_NestedLoops_3 {
 	 */
 	public static void main(String[] args) {
 		Calendar cal=Calendar.getInstance();
-		int day = cal.get(Calendar.DAY_OF_WEEK);
-		int d = 0;
+		int d = 1;
 		int x;
+		int currentDayOfMonth = cal.get(Calendar.DAY_OF_MONTH);
 		int m = Integer.parseInt(JOptionPane.showInputDialog("Enter a month number"));
 		int y = Integer.parseInt(JOptionPane.showInputDialog("Enter a year"));
 		cal.set(y,(m+1), 1);
@@ -25,24 +25,25 @@ public class HC_SM_NestedLoops_3 {
 		System.out.print("  ");
 		System.out.print(y);
 		System.out.println();
-		System.out.println("S  M  Tu  W  Th F  S");
+		System.out.println(" S  M  Tu W  Th F  S");
 		for (int line = 1; line <= 5; line++) {
-			for (int num = 1; num <= (line); num++) {
-				d = num;
-			}
 			for (int num = 1; num <= (cal.getFirstDayOfWeek()+7); num++) {
+				for (int day = 0; day <= (7 - cal.get(Calendar.DAY_OF_WEEK)); day++)	{
+					cal.set(y,(m+1), d);
+					d++;
 				if (Integer.toString(d).length() == 1)	{
-					if (d > 1)
+					if (d > 0)
 						System.out.print(" " + dual.format(d)+ " ");
 					else
 						System.out.print(" " + " " + " ");
 				}
 				else	{
 					System.out.print(dual.format(d)+ " ");
-					if (d > 1)
+					if (d > 0)
 						System.out.print(" " + dual.format(d)+ " ");
 					else
 						System.out.print(" " + "  " + " ");
+				}
 				}
 			}
 			System.out.println();
