799edfcdd1c86b11ce40d46ff01ebc62395fbf70
Mithon <huntercaron@mac.com>
2013-10-28 19:26:11 -0400

    Almost Works now
    Getting the first day of the week should work, but I think its feeding me the wrong info

--- HC_SE_LoopsCalendar.java
+++ HC_SE_LoopsCalendar.java
@@ -10,16 +10,20 @@ public class HC_SM_NestedLoops_3 {
 	 * @author Sherif and Hunter
 	 */
 	public static void main(String[] args) {
-		Calendar cal=Calendar.getInstance();
-		int d = 1;
-		int x;
-		int currentDayOfMonth = cal.get(Calendar.DAY_OF_MONTH);
-		int m = Integer.parseInt(JOptionPane.showInputDialog("Enter a month number"));
-		int y = Integer.parseInt(JOptionPane.showInputDialog("Enter a year"));
+		Calendar cal=Calendar.getInstance(TimeZone.getDefault());
+		int m = 8; //Integer.parseInt(JOptionPane.showInputDialog("Enter a month number"));
+		int y = 2013;//Integer.parseInt(JOptionPane.showInputDialog("Enter a year"));
 		cal.set(y,(m+1), 1);
+		int begin = cal.get(Calendar.DAY_OF_WEEK);
 		DecimalFormat dual = new DecimalFormat("##");
 		String[] months = new java.text.DateFormatSymbols().getMonths();
-
+		int d = begin-6;
+		//days in month
+		int[] dim = {31,28,31,30,31,30,31,31,30,31,30,31};
+		if (y%4 == 0)
+			dim[1] = 29;
+		int end = dim[m+1];
+		
 		System.out.print("    ");
 		System.out.print(months[m-1]);
 		System.out.print("  ");
@@ -27,24 +31,17 @@ public class HC_SM_NestedLoops_3 {
 		System.out.println();
 		System.out.println(" S  M  Tu W  Th F  S");
 		for (int line = 1; line <= 5; line++) {
-			for (int num = 1; num <= (cal.getFirstDayOfWeek()+7); num++) {
-				for (int day = 0; day <= (7 - cal.get(Calendar.DAY_OF_WEEK)); day++)	{
-					cal.set(y,(m+1), d);
-					d++;
-					currentDayOfMonth = cal.get(Calendar.DAY_OF_MONTH);
-				if (Integer.toString(d).length() == 1)	{
-					if (d > 0)
-						System.out.print(" " + dual.format(currentDayOfMonth)+ " ");
-					else
-						System.out.print(" " + " " + " ");
+			for (int day = 1; day <= 7; day++)	{
+				if (d-1 == end)
+					break;
+				if (d > 0)	{
+					if (d<=9)
+						System.out.print(" ");
+					System.out.print(dual.format(d)+ " ");
 				}
-				else	{
-					if (d > 0)
-						System.out.print(" " + dual.format(currentDayOfMonth)+ " ");
-					else
-						System.out.print(" " + "  " + " ");
-				}
-				}
+				else
+					System.out.print(" " + " " + " ");
+				d++;
 			}
 			System.out.println();
 		}
