8a1c12511c5666f28103693fa42bf79d74a329cd
Mithon <huntercaron@mac.com>
2013-11-12 14:41:58 -0500

    workish

--- Methods10d_HC.java
+++ Methods10d_HC.java
@@ -1,4 +1,4 @@
-import java.awt.Color;
+import java.awt.*;
 
 
 public class Methods10d_HC {
@@ -9,21 +9,27 @@ public class Methods10d_HC {
 	 * Methods 10b
 	 */
 	public static void main(String[] args) {
-		Ball(1,10,Color.black,0);
-		Ball(1,10,Color.red,10);
-
+		Frame f = new Frame();
+		f.setSize(300,300);                         
+		f.setVisible(true); 
+		f.setBackground(Color.white);
+		Graphics g = f.getGraphics();
+		
+		Ball(g,1,10,Color.LIGHT_GRAY,0);
+		Ball(g,1,10,Color.BLUE,30);
 	}
-	public static void Ball(int ball,int BallW,Color c,int d)	{
+	public static void Ball(Graphics g, int ball,int BallW,Color c,int d)	{
 		Ball b= new Ball();
 		b.delay(d);
-		for (int i=1; i<100; i++)	{
-			b.erase();
+		for (int i=1; i<300; i++)	{
+
+			b.erase(g);
 
 			b.move();
 
-			b.draw(c);
+			b.draw(c,g);
 
-			b.delay(100);
+			b.delay(50);
 		}
 		System.exit(0); 
 	}
