d0d49f12795ffaa299f0d4306130ee8ab4263c8a
Mithon <huntercaron@mac.com>
2013-11-12 14:41:31 -0500

    workish

--- Ball.java
+++ Ball.java
@@ -1,64 +1,62 @@
 import java.awt.*;
 
-class Ball extends Frame
+class Ball 
 {
-   Graphics graphics=null;
 
-   static final int WIDTH = 20;                        // Ball height, width
-   static final int HEIGHT = 20;
+	public static final int WIDTH = 20;                        // Ball height, width
+	public static final int HEIGHT = 20;
 
-   int x = 150;                                              // Current x,y
-   int y = 60;
-                      
-   int dx = 10;                                               // Distance moved
-   int dy = 10; 
+	public int x = 150;                                              // Current x,y
+	public int y = 60;
 
-   public Ball()
-   { 
-      setSize(300,300);                                  // Size
-      setVisible(true); 
-      setBackground(Color.white);                     // Color
-      graphics = getGraphics();                       // Frame reference
-   } 
+	public int dx = 10;                                               // Distance moved
+	public int dy = 10; 
 
-   public void paint(Graphics graphics) {      
-       draw(Color.black);
-   }
-    
-   public void erase() {
-      graphics.setColor(Color.white);                // Erase ball
-      graphics.fillOval(x,y,WIDTH,HEIGHT); 
-   }
+	public Ball()
+	{ 
+		
+		                
+	} 
 
-   public void draw(Color c) {
-      graphics.setColor(c);                   // Draw ball 
-      graphics.fillOval(x,y,WIDTH,HEIGHT);
-   }
+	public void paint(Graphics g) {      
+		draw(Color.black,g);
+	}
 
-public void delay(int t)
-{
-	try { Thread.sleep(t); } catch(Exception e) {}
+	public void erase(Graphics g) {
+		g.setColor(Color.white);                // Erase ball
+		g.fillOval(x,y,WIDTH,HEIGHT); 
+	}
+
+	public void draw(Color c,Graphics g) {
+		g.setColor(c);                   // Draw ball 
+		g.fillOval(x,y,WIDTH,HEIGHT);
+	}
+
+	public void delay(int t)
+	{
+		try { Thread.sleep(t); } catch(Exception e) {}
+	}
+	public void move() {  
+		x = x + dx; 											 // Move x
+		y = y + dy;                                            // Move y
+
+		if (x < 0) {                                            // Hit left wall
+			x = 0; 
+			dx = -dx; 
+		}                  
+/**
+		if (x + WIDTH >= getWidth()) {               // Hit right wall
+			x = getWidth() - WIDTH;             
+			dx = -dx;
+		} 
+		if (y < 25) {                                            // Hit top wall
+			y = 25; 
+			dy = -dy; 
+		}                                                           
+		if (y + HEIGHT >= getHeight()) {             // Hit bottom wall
+			y = getHeight() - HEIGHT; 
+			dy = -dy; 
+		}   **/
+	}
 }
-   public void move() {  
-      x = x + dx; 											 // Move x
-      y = y + dy;                                            // Move y
 
-      if (x < 0) {                                            // Hit left wall
-          x = 0; 
-          dx = -dx; 
-      }                  
-
-      if (x + WIDTH >= getWidth()) {               // Hit right wall
-          x = getWidth() - WIDTH;             
-          dx = -dx;
-      } 
-      if (y < 25) {                                            // Hit top wall
-          y = 25; 
-          dy = -dy; 
-      }                                                           
-      if (y + HEIGHT >= getHeight()) {             // Hit bottom wall
-          y = getHeight() - HEIGHT; 
-          dy = -dy; 
-      }   
-   }
-}
