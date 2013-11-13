3c20c586060c27d2cb747cadb2095c8051fb84f7
Mithon <huntercaron@mac.com>
2013-11-13 14:47:26 -0500

    works

--- Methods10d_HC.java
+++ Methods10d_HC.java
@@ -10,31 +10,41 @@ public class Methods10d_HC {
 	 */
 	public static void main(String[] args) {
 		Frame f = new Frame();
-		f.setSize(300,300);                         
+		f.setSize(500,500);                         
 		f.setVisible(true); 
 		f.setBackground(Color.white);
 		Graphics g = f.getGraphics();
-		Ball b = new Ball();
-		Ball c = new Ball();
-		
-		
-		Ball(g,b,Color.LIGHT_GRAY,0,f, 150, 60);
-		Ball(g,c,Color.BLUE,100,f, 0, 0);
+
+		Ball(g, Color.LIGHT_GRAY, Color.GRAY, f, 150, 60, 500, 60);
 	}
-	public static void Ball(Graphics g, Ball b, Color c,int d,Frame f,int x, int y)	{
-		b.delay(d);
+	public static void Ball(Graphics g, Color c, Color c2, Frame f,int x, int y,int x2, int y2)        {
+		Ball b = new Ball();
+		Ball a = new Ball();
 		b.startLOC(x,y);
-		for (int i=1; i<300; i++)	{
-
-			b.erase(g);
-
-			b.move(f);
-
-			b.draw(c,g);
-
-			b.delay(50);
+		a.startLOC(x2,y2);
+		double d = 100;
+		for (int i=1; i<200; i++)        {
+			move(g,c,f,b);
+			move(g,c2,f,a);
+			d = Math.sqrt((a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y));
+			if (d<50)	{
+				a.dx = -a.dx;
+				a.dy = -a.dy;
+				b.dx = -b.dx;
+				b.dy = -b.dy;
+			}	
 		}
 		System.exit(0); 
 	}
+	public static void move(Graphics g, Color c, Frame f, Ball b)	{
+		b.erase(g);
+
+		b.move(f);
+
+		b.draw(c,g);
+
+		b.delay(30);
+	}
+
 
 }
