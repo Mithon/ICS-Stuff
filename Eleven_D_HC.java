82ff253cbfb7705faafb1e655dd87c41f62bd24a
Mithon <huntercaron@mac.com>
2013-12-08 10:16:45 -0500

    added optimized profanity cleaner
    so much better than my old version of it

--- Eleven_D_HC.java
+++ Eleven_D_HC.java
@@ -1,12 +1,13 @@
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Arrays;
-import javax.swing.BorderFactory;
+import java.util.ArrayList;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 
@@ -24,8 +25,20 @@ public class Eleven_D_HC {
 		final JFrame frame = new JFrame();
 		frame.setBounds(450, 200, 600, 106);
 		frame.getContentPane().setLayout(null);
-		frame.setTitle("Hunter's Profanity Cleaner and Pattern Maker");
+		frame.setTitle("Hunter & Sherif's Profanity Cleaner and Pattern Maker");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		final JFrame output = new JFrame();
+		output.setBounds(450, 200, 400, 300);
+		output.getContentPane().setLayout(null);
+		output.setTitle("Output");
+		output.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		//output text field
+		final JTextArea print = new JTextArea();
+		print.setSize(output.getSize());
+		print.setEditable(false);
+		output.add(print);
 
 		//input 1 label
 		JLabel label1 = new JLabel("Input:");
@@ -45,38 +58,53 @@ public class Eleven_D_HC {
 
 		profanity.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				String input = field.getText();
-				String output;
-				output = cleaner(input,"fuck");
-				System.out.print(output);
+				//easily expandable profanity cleaner
+				if (field.getText().equals(""))
+					JOptionPane.showMessageDialog(null, "Please enter text into the field","Input Error",JOptionPane.ERROR_MESSAGE);
+				//string array for bad words that can be easily added to
+				//last word in array will not be read
+				String[] bad = {"fuck", "penis", "cunt", "bitch", "whore", null};
+				//array list for each version after a word is taken out
+				ArrayList<String> versions = new ArrayList<String>();
+				versions.add(field.getText());
+				//loop that removes one bad word from current string using the cleaner method and assigns it to a new string, then repeats
+				//loop runs once per bad word
+				for (int i = 1; i < bad.length;i++ )	{
+					versions.add(cleaner(versions.get(i-1), bad[i-1]));
+				}
+				//gets last version of string that is fully cleaned
+				print.setText(versions.get(versions.size()-1));
+				output.setVisible(true);
 			}
 		});
-		
+
 		final JButton diamond = new JButton("Make Diamond");
 		diamond.setBounds(195,40,120,19);
 		frame.add(diamond);
 
 		diamond.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				String input = field.getText().replaceAll("\\W+","").toUpperCase();
-				int len = input.length();
-				char[] word = input.toCharArray();
-				for (int line = 0; line < len; line++)	{
-					for (int x = 0; x >= (len-1)*-2; x--)	{
-						System.out.print(" ");
+				if (field.getText().equals(""))
+					JOptionPane.showMessageDialog(null, "Please enter text into the field","Input Error",JOptionPane.ERROR_MESSAGE);
+				StringBuffer diamond = new StringBuffer();
+				String x = field.getText().replaceAll("\\W+","").toUpperCase();
+				for(int line=0;line<=x.length()-1;line++){
+					for(int space=x.length()-1;space>=line;space--){
+						diamond.append(" ");
 					}
-					for (int i = 0; i < 1; i++)	{
-						System.out.print(word[line]);				
+					int c= line;
+					diamond.append(x.charAt(c));
+					for(int space=0;space<=2*line-2;space++){
+						diamond.append(" ");
 					}
-					for (int x = 0; x <= (len-1); x++)	{
-						System.out.print(" ");
+					if(c!=0){
+						diamond.append(x.charAt(c));
 					}
-					if (line != 0)	{
-					for (int i = 0; i < 1; i++)	{
-						System.out.print(word[line]);				
-					}
-					}
-					System.out.println("");
+					c++;
+
+					diamond.append("\n");
+					print.setText(diamond.toString());
+					output.setVisible(true);
 				}
 			}
 		});
@@ -95,7 +123,7 @@ public class Eleven_D_HC {
 
 		frame.setVisible(true);
 	}
-	public static String cleaner(String str, String profanity)	{
+	public static String cleaner(String str, String profanity)        {
 		int len = profanity.length();
 		StringBuffer bleep = new StringBuffer("");
 		for (int i = 0; i < len; i++)
