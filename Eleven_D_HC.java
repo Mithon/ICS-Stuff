934846cdd6d8c02515b4e418350abaad996afaee
Mithon <huntercaron@mac.com>
2013-12-09 14:46:06 -0500

    diamond

--- Eleven_D_HC.java
+++ Eleven_D_HC.java
@@ -13,123 +13,143 @@ import javax.swing.JTextField;
 
 public class Eleven_D_HC {
 
-	/**
-	 * @author Hunter Caron
-	 * Mr.Marco
-	 * Strings Assignment 11a
-	 * part 2
-	 */
+        /**
+         * @author Hunter Caron
+         * Mr.Marco
+         * Strings Assignment 11d
+         */
 
-	public static void main(String[] args) {
-		//creating frame
-		final JFrame frame = new JFrame();
-		frame.setBounds(450, 200, 600, 106);
-		frame.getContentPane().setLayout(null);
-		frame.setTitle("Hunter & Sherif's Profanity Cleaner and Pattern Maker");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        public static void main(String[] args) {
+                //creating frame
+                final JFrame frame = new JFrame();
+                frame.setBounds(450, 200, 600, 106);
+                frame.getContentPane().setLayout(null);
+                frame.setTitle("Hunter & Sherif's Profanity Cleaner and Pattern Maker");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		final JFrame output = new JFrame();
-		output.setBounds(450, 200, 400, 300);
-		output.getContentPane().setLayout(null);
-		output.setTitle("Output");
-		output.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                //output window
+                final JFrame output = new JFrame();
+                output.setBounds(450, 200, 400, 300);
+                output.getContentPane().setLayout(null);
+                output.setTitle("Output");
+                output.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		//output text field
-		final JTextArea print = new JTextArea();
-		print.setSize(output.getSize());
-		print.setEditable(false);
-		output.add(print);
+                //output text field
+                final JTextArea print = new JTextArea();
+                print.setSize(output.getSize());
+                print.setEditable(false);
+                print.setFont((new Font(null, Font.BOLD, 14)));
+                output.add(print);
 
-		//input 1 label
-		JLabel label1 = new JLabel("Input:");
-		label1.setBounds(15,15,60,15);
-		label1.setFont((new Font(null, Font.BOLD, 14)));
-		frame.add(label1);
+                //input 1 label
+                JLabel label1 = new JLabel("Input:");
+                label1.setBounds(15,15,60,15);
+                label1.setFont((new Font(null, Font.BOLD, 14)));
+                frame.add(label1);
 
-		//input box
-		final JTextField field = new JTextField();
-		field.setBounds(65,14,500,20);
-		frame.add(field);
+                //input box
+                final JTextField field = new JTextField();
+                field.setBounds(65,14,500,20);
+                frame.add(field);
 
-		//initials button
-		final JButton profanity = new JButton("Clear Profanity");
-		profanity.setBounds(65,40,120,19);
-		frame.add(profanity);
+                //initials button
+                final JButton profanity = new JButton("Clear Profanity");
+                profanity.setBounds(65,40,120,19);
+                frame.add(profanity);
 
-		profanity.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				//easily expandable profanity cleaner
-				if (field.getText().equals(""))
-					JOptionPane.showMessageDialog(null, "Please enter text into the field","Input Error",JOptionPane.ERROR_MESSAGE);
-				//string array for bad words that can be easily added to
-				//last word in array will not be read
-				String[] bad = {"fuck", "penis", "cunt", "bitch", "whore", null};
-				//array list for each version after a word is taken out
-				ArrayList<String> versions = new ArrayList<String>();
-				versions.add(field.getText());
-				//loop that removes one bad word from current string using the cleaner method and assigns it to a new string, then repeats
-				//loop runs once per bad word
-				for (int i = 1; i < bad.length;i++ )	{
-					versions.add(cleaner(versions.get(i-1), bad[i-1]));
-				}
-				//gets last version of string that is fully cleaned
-				print.setText(versions.get(versions.size()-1));
-				output.setVisible(true);
-			}
-		});
+                profanity.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent arg0) {
+                            //easily expandable profanity cleaner
+                            if (field.getText().equals(""))
+                                    JOptionPane.showMessageDialog(null, "Please enter text into the field","Input Error",JOptionPane.ERROR_MESSAGE);
+                            //string array for bad words that can be easily added to
+                            //last word in array will not be read
+                            String[] bad = {"fuck", "penis", "cunt", "bitch", "whore", "twat", "nigger", "nigga", "chink", "wigger", "ass", "shit", "slut", "faggot", "dick", null};
+                            //array list for each version after a word is taken out
+                            ArrayList<String> versions = new ArrayList<String>();
+                            versions.add(field.getText());
+                            //loop that removes one bad word from current string using the cleaner method and assigns it to a new string, then repeats
+                            //loop runs once per bad word
+                            for (int i = 1; i < bad.length;i++ )        {
+                                    versions.add(cleaner(versions.get(i-1), bad[i-1]));
+                            }
+                            //gets last version of string that is fully cleaned
+                            print.setText(versions.get(versions.size()-1));
+                            output.setVisible(true);
+                    }
+            });
 
-		final JButton diamond = new JButton("Make Diamond");
-		diamond.setBounds(195,40,120,19);
-		frame.add(diamond);
+                final JButton diamond = new JButton("Make Diamond");
+                diamond.setBounds(195,40,120,19);
+                frame.add(diamond);
 
-		diamond.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				if (field.getText().equals(""))
-					JOptionPane.showMessageDialog(null, "Please enter text into the field","Input Error",JOptionPane.ERROR_MESSAGE);
-				StringBuffer diamond = new StringBuffer();
-				String x = field.getText().replaceAll("\\W+","").toUpperCase();
-				for(int line=0;line<=x.length()-1;line++){
-					for(int space=x.length()-1;space>=line;space--){
-						diamond.append(" ");
-					}
-					int c= line;
-					diamond.append(x.charAt(c));
-					for(int space=0;space<=2*line-2;space++){
-						diamond.append(" ");
-					}
-					if(c!=0){
-						diamond.append(x.charAt(c));
-					}
-					c++;
+                diamond.addActionListener(new ActionListener() {
+                        public void actionPerformed(ActionEvent arg0) {
+                                if (field.getText().equals(""))
+                                        JOptionPane.showMessageDialog(null, "Please enter text into the field","Input Error",JOptionPane.ERROR_MESSAGE);
+                                StringBuffer diamond = new StringBuffer();
+                                String x = field.getText().replaceAll("\\W+","").toUpperCase();
+                                for(int line=0;line<=x.length()-1;line++){
+                                        for(int space=x.length()-1;space>=line;space--){
+                                                diamond.append(" ");
+                                        }
+                                        int c= line;
+                                        diamond.append(x.charAt(c));
+                                        for(int space=0;space<=2*line-2;space++){
+                                                diamond.append(" ");
+                                        }
+                                        if(c!=0){
+                                                diamond.append(x.charAt(c));
+                                        }
+                                        c++;
+                                        diamond.append("\n");
+                                }
+                                for(int line=x.length()-2;line>=0;line--){
+                                    
+                                    for(int space=x.length()-1;space>=line;space--){
+                                            diamond.append(" ");
+                                    }
+                                    
+                                    int c= line;
+                                                    diamond.append(x.charAt(c));
+                                                    for(int space=0;space<=2*line-2;space++){
+                                                            diamond.append(" ");
+                                                    }
+                                                    if(c!=0){
+                                                            diamond.append(x.charAt(c));
+                                                    }
+                                            c--;
+                                    
+                                    diamond.append("\n");
+                          }
+                                print.setText(diamond.toString());
+                                output.setVisible(true);
+                        }
+                });
 
-					diamond.append("\n");
-					print.setText(diamond.toString());
-					output.setVisible(true);
-				}
-			}
-		});
+                //adds clear button
+                final JButton clear = new JButton("Clear Field");
+                clear.setBounds(325,40,120,19);
+                frame.add(clear);
 
-		//adds clear button
-		final JButton clear = new JButton("Clear All");
-		clear.setBounds(210,145,110,32);
-		frame.add(clear);
+                clear.addActionListener(new ActionListener() {
+                        public void actionPerformed(ActionEvent arg0) {
+                                //sets the field to empty
+                                field.setText("");
+                        }
+                });
 
-		clear.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				//sets the field to empty
-				field.setText("");
-			}
-		});
-
-		frame.setVisible(true);
-	}
-	public static String cleaner(String str, String profanity)        {
-		int len = profanity.length();
-		StringBuffer bleep = new StringBuffer("");
-		for (int i = 0; i < len; i++)
-			bleep.append("*");
-		String output = str.replaceAll(profanity, bleep.toString());
-		return output;
-	}
+                frame.setVisible(true);
+        }
+        public static String cleaner(String str, String profanity)        {
+            int len = profanity.length();
+            StringBuffer bleep = new StringBuffer("");
+            for (int i = 0; i < len; i++)
+                    bleep.append("*");
+            String output = str.replaceAll(profanity, bleep.toString());
+            if (output.equals(str))
+                JOptionPane.showMessageDialog(null, "You will be sent to the office","Profanity Detected",JOptionPane.ERROR_MESSAGE);
+            return output;
+    }
 
 }
